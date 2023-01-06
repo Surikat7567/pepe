03d2cd4ea9d24bb061be8299459221409c4f078d
Surikat7567 <114401572+Surikat7567@users.noreply.github.com>
2023-01-06 19:38:16 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
             Graphics2D g2 = (Graphics2D)g;
             try {
                 URL url = new URL("https://images.squarespace-cdn.com/content/v1/5bf85ceb9772aeb688fe5f4b/1570296207268-U4NY4ZXJEYNV056X738Q/20190523_202042+-+wm.jpg?format=1000w");
-                Image drm_im = new ImageIcon("C:\\Users\\Admin\\Desktop\\KPI\\Kurs4ch\\drm\\src\\drm.png").getImage();
+                Image drm_im = new ImageIcon("C:\\pepe-Drums-2\\src\\drm.png").getImage();
                 g2.drawImage(drm_im,0,0, 1000, 486, null);
             }catch (MalformedURLException e){
                 e.printStackTrace();
@@ -41,7 +41,7 @@ public class Main {
         button.setBounds(181,130,70,30);
         button.addActionListener(e -> {
             try {
-                File soundFile = new File("C:\\Users\\Admin\\Desktop\\KPI\\Kurs4ch\\drm\\src\\hihat_cl.wav"); //Звуковий файл
+                File soundFile = new File("C:\\pepe-Drums-2\\src\\hihat_cl.wav"); //Звуковий файл
 
                 AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);
 
@@ -61,7 +61,7 @@ public class Main {
         crashbutton.setBounds(241,40,70,30);
         crashbutton.addActionListener(e -> {
             try {
-                File csoundFile = new File("C:\\Users\\Admin\\Desktop\\KPI\\Kurs4ch\\drm\\src\\crash.wav"); //Звуковий файл
+                File csoundFile = new File("C:\\pepe-Drums-2\\src\\crash.wav"); //Звуковий файл
 
                 AudioInputStream cais = AudioSystem.getAudioInputStream(csoundFile);
 
@@ -78,7 +78,7 @@ public class Main {
         rbutton.setBounds(541,80,70,30);
         rbutton.addActionListener(e -> {
             try {
-                File rsoundFile = new File("C:\\Users\\Admin\\Desktop\\KPI\\Kurs4ch\\drm\\src\\R.wav"); //Звуковий файл
+                File rsoundFile = new File("C:\\pepe-Drums-2\\src\\R.wav"); //Звуковий файл
 
                 AudioInputStream rais = AudioSystem.getAudioInputStream(rsoundFile);
 
@@ -95,7 +95,7 @@ public class Main {
         r2button.setBounds(721,70,70,30);
         r2button.addActionListener(e -> {
             try {
-                File r2soundFile = new File("C:\\Users\\Admin\\Desktop\\KPI\\Kurs4ch\\drm\\src\\RR.wav"); //Звуковий файл
+                File r2soundFile = new File("C:\\pepe-Drums-2\\src\\RR.wav"); //Звуковий файл
 
                 AudioInputStream r2ais = AudioSystem.getAudioInputStream(r2soundFile);
 
@@ -112,7 +112,7 @@ public class Main {
         sbutton.setBounds(333,210,70,30);
         sbutton.addActionListener(e -> {
             try {
-                File SsoundFile = new File("C:\\Users\\Admin\\Desktop\\KPI\\Kurs4ch\\drm\\src\\SSSnare.wav"); //Звуковий файл
+                File SsoundFile = new File("C:\\pepe-Drums-2\\src\\SSSnare.wav"); //Звуковий файл
 
                 AudioInputStream sais = AudioSystem.getAudioInputStream(SsoundFile);
 
@@ -129,7 +129,7 @@ public class Main {
         Htbutton.setBounds(330,123,75,30);
         Htbutton.addActionListener(e -> {
             try {
-                File HsoundFile = new File("C:\\Users\\Admin\\Desktop\\KPI\\Kurs4ch\\drm\\src\\HiTom.wav"); //Звуковий файл
+                File HsoundFile = new File("C:\\pepe-Drums-2\\src\\HiTom.wav"); //Звуковий файл
 
                 AudioInputStream Htais = AudioSystem.getAudioInputStream(HsoundFile);
 
@@ -146,7 +146,7 @@ public class Main {
         Ltbutton.setBounds(560,222,70,30);
         Ltbutton.addActionListener(e -> {
             try {
-                File LtsoundFile = new File("C:\\Users\\Admin\\Desktop\\KPI\\Kurs4ch\\drm\\src\\LTom.wav"); //Звуковий файл
+                File LtsoundFile = new File("C:\\pepe-Drums-2\\src\\LTom.wav"); //Звуковий файл
 
                 AudioInputStream Ltais = AudioSystem.getAudioInputStream(LtsoundFile);
 
@@ -163,7 +163,7 @@ public class Main {
         Ftbutton.setBounds(650,300,70,30);
         Ftbutton.addActionListener(e -> {
             try {
-                File FtsoundFile = new File("C:\\Users\\Admin\\Desktop\\KPI\\Kurs4ch\\drm\\src\\FTom.wav"); //Звуковий файл
+                File FtsoundFile = new File("C:\\pepe-Drums-2\\src\\FTom.wav"); //Звуковий файл
 
                 AudioInputStream Ftais = AudioSystem.getAudioInputStream(FtsoundFile);
 
@@ -180,7 +180,7 @@ public class Main {
         bassbutton.setBounds(448,300,70,30);
         bassbutton.addActionListener(e -> {
             try {
-                File bassSoundFile = new File("C:\\Users\\Admin\\Desktop\\KPI\\Kurs4ch\\drm\\src\\Bass.wav"); //Звуковий файл
+                File bassSoundFile = new File("C:\\pepe-Drums-2\\src\\Bass.wav"); //Звуковий файл
 
                 AudioInputStream bass = AudioSystem.getAudioInputStream(bassSoundFile);
 
